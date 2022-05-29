9c519c22d781f459e0342a92f42a70e508ce2168
Ruann Godinho <ruann.godinho@fatec.sp.gov.br>
2022-05-29 13:42:08 -0300

    Projeto-Rpg

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Projeto_Rpg.src.entities;
+
+namespace Projeto_Rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight("Arus", 25, "Knight");
+            Wizard wizard = new Wizard("Jennica", 25, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(knight.Attack());
+        }
+    }
+}
---
+++ src/entities/Knight.cs
@@ -0,0 +1,12 @@
+namespace Projeto_Rpg.src.entities
+{
+    public class Knight : Hero
+    {
+        public Knight(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+    }
+}
